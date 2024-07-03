ef8d55ca77aecee58ebaaaa16c6abf01a6adf41f
Rafi Ferdos <rafiferdos@gmail.com>
2024-07-03 21:14:39 +0600

    refactor: Update Projects component to use lazy loading for images and improve layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
@@ -9,7 +9,11 @@ import { useContext } from "react";
 export default function Home() {
   const { theme } = useContext(ThemeContext);
   return (
-    <div className={`min-h-screen antialiased lg:space-y-24 md:space-y-20 space-y-16 ${theme === 'dark' ? 'bg-black' : 'bg-white'}`}>
+    <div
+      className={`min-h-screen antialiased lg:space-y-24 md:space-y-20 space-y-16 ${
+        theme === "dark" ? "bg-black" : "bg-white"
+      }`}
+    >
       <Hero />
       <Skills />
       <Projects />
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -53,11 +53,13 @@ const Projects = () => {
           }}
           className="div"
         >
-          <div className={
-            theme === 'dark' 
-            ? "h-[20rem] md:h-[30rem] w-full bg-black dark:bg-grid-white/[0.2] bg-grid-white/[0.2] relative flex items-center justify-center"
-            : "h-[20rem] md:h-[30rem] w-full bg-white dark:bg-grid-black/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center"
-          }>
+          <div
+            className={
+              theme === "dark"
+                ? "h-[20rem] md:h-[30rem] w-full bg-black dark:bg-grid-white/[0.2] bg-grid-white/[0.2] relative flex items-center justify-center"
+                : "h-[20rem] md:h-[30rem] w-full bg-white dark:bg-grid-black/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center"
+            }
+          >
             <div
               className={
                 theme === "dark"
@@ -66,11 +68,13 @@ const Projects = () => {
               }
             ></div>
             <div className="flex flex-col items-center justify-center px-8">
-              <p className={
-                theme === 'dark'
-                ? "text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500"
-                : "text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-500"
-              }>
+              <p
+                className={
+                  theme === "dark"
+                    ? "text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500"
+                    : "text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-500"
+                }
+              >
                 Projects
               </p>
               <div className="text-center text-base md:text-lg font-normal text-neutral-500 max-w-2xl mx-auto">
