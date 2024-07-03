32108aa93d19211cd50c209d533879b3db3763e7
Rafi Ferdos <rafiferdos@gmail.com>
2024-07-03 22:51:30 +0600

    refactor: Improve scrollToComponent function and remove redundant code

--- src/components/scrollToComponent.tsx
+++ src/components/scrollToComponent.tsx
@@ -1,10 +1,8 @@
 'use client'
+
 export function scrollToComponent(id: string) {
-  // Check if running in a browser environment
-  if (typeof window !== 'undefined') {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
   }
 }
